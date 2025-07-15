e16a3d17ea22d89e8317470cc3cb097caa17aab9
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-15 22:32:45 +0500

    Added a text toggle example of use state

--- src/pages/Use-state.jsx
+++ src/pages/Use-state.jsx
@@ -8,20 +8,47 @@ function UseState() {
     
     let [num,setNum]=useState(90);
     let [isDisabled,setIsDisabled]=useState(false);
+    // let [count,setCount]=useState(0);
+    let [text,setText]=useState("I am learning react");
+    let [toggle,setToggle]=useState(true);
 
     function onClick(){
-        if(num>0){
-            setNum(num-6);
+            const newNum = num - 6;
+            setNum(newNum);
+            
+            if (newNum <= 0) {
+              setIsDisabled(true);
+            }
+          
+          
+        // const newCount=count+1;
+        // setCount(newCount);
+        // if(newCount>=3){
+        //     setIsDisabled(true);
+        
+
+
         }
-        if(num<=0){
-            setIsDisabled(true);
+        function handleClick(){
+            setToggle(!toggle);
+        if(toggle){
+            setText("React is fun");
         }
-    }
+        else{
+            setText("I am learning react");
+        }
+        }
+        
+    
 
     return (
         <>
             <p>{num}</p>
             <button onClick={onClick} disabled={isDisabled}>Click</button>
+            <p>{text}</p>
+            <button onClick={handleClick}>Change Text</button>
+
+
 
         </>
     )
