dbe306f32e9804405ea80f8cc16b05c3b2705000
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-15 20:53:06 +0500

    Getting familiar with react syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  let a=90;
 
+  function handleClick(){
+    a++;
+    console.log(a);
+    document.getElementById("p1").innerHTML=a;//this is used to update the value of the p tag
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Home/>
+    <About/>
+    <Contact/>
+    <p id="p1">{a}</p> {/* this is used to display the value of the p tag. {} is used to display the value of the variable a */}
+    <button onClick={handleClick}>click</button> {/* no () is used to call the function handleClick coz it executes the function before the button is clicked . treated like a callback function */}
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+   
+    <>
+    <h2>hello world</h2>
+    <p>data</p>
+    </>
+    
+  )
+}
+
+export default Home
