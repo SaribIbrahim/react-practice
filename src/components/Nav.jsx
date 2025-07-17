e02846338247cdb3b7688e9949457f29334a8f25
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-17 17:43:39 +0500

    Added NavLinks to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link,NavLink} from "react-router-dom";
 
 function Nav({products}) {
     return (
@@ -6,26 +7,29 @@ function Nav({products}) {
 
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <Link className="navbar-brand" to={"/"}>Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <NavLink className="nav-link" aria-current="page" to={"/about"}>About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
+                                <NavLink className="nav-link" to={"/contact"}>Contact</NavLink>
                             </li>
-                            
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
+                                <NavLink className="nav-link" to={"/parentlist"}>ParentList</NavLink>
                             </li>
                         </ul>
                         <ul>
-                            <strong className="ms-auto">{products.length}</strong>
+                            <button className="btn btn-info me-2">Sign In</button>
                         </ul>
+                        <ul>
+                            <strong>{products.length}</strong>
+                        </ul>
+
                     </div>
                 </div>
             </nav>
