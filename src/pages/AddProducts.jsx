782b827d28d47549e2d9a45d5e59c866e39da401
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-19 19:16:31 +0500

    Form handling and validation through formik and yup without image

--- src/pages/AddProducts.jsx
+++ src/pages/AddProducts.jsx
@@ -1,46 +1,57 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React from 'react'
+import {  useFormik } from 'formik'
+import * as Yup from 'yup'
+
 
 function AddProducts() {
-
-    let [product, setProduct] = useState({
-        title: "",
-        description: "",
-        price: undefined,
-        image: "",
-        category: ""
-    })  
-    
-   let handleSubmit=async(e)=>{
-   e.preventDefault();
-  let response=await axios.post('https://fakestoreapi.com/products',product)//product:product
-  console.log(response);
-   
-   }
+  
+   let {handleChange,handleSubmit,values,errors,touched,handleReset,handleBlur} = useFormik({
+    initialValues:{
+        title:"",
+        description:"",
+        price:"",
+        // image:"",
+        category:""
+    },
+    validationSchema:Yup.object().shape({
+        title:Yup.string("Title must be a string").required("Title is required").min(5,"Title must be alteast 5 characters"),
+        description:Yup.string("Description must be a string").required("Description is required"),
+        price:Yup.number().typeError("Price must be a number").required("Price is required").min(10,"Minimum price must be $10"),
+        // image:Yup.string("Image must be a string").required("Image is required"),
+        category:Yup.string().required("Category is required")
+    }),
+    onSubmit:(vals,{resetForm})=>{
+        console.log(vals);
+        resetForm();
+    }
+   });
 
     return (
         <div className='w-50 p-5'>
-            <form className="row g-3" onSubmit={handleSubmit}>
-                <div className="col-12">
-                    <label for="Title" className="form-label">Title</label>
-                    <input onChange={(e) => { setProduct({ ...product, title: e.target.value }) }} name='title' type="text" className="form-control" id="Title" />
+            <form className="row g-3" onSubmit={handleSubmit} onReset={handleReset}>
+                <div className="col-12 ">
+                    <label htmlFor="Title" className="form-label">Title</label>
+                    <input name='title' type="text" className="form-control" id="Title" onChange={handleChange} onBlur={handleBlur} value={values.title}/>
                 </div>
+                <div className="error mt-0" style={{color:"red"}}>{touched.title &&  errors.title?errors.title:""}</div>
                 <div className="col-12">
-                    <label for="Description" className="form-label">Description</label>
-                    <textarea onChange={(e) => { setProduct({ ...product, description: e.target.value }) }} name='description' type="text" className="form-control" id="Description" />
+                    <label htmlFor="Description" className="form-label">Description</label>
+                    <textarea  name='description' type="text" className="form-control" id="Description" onChange={handleChange} onBlur={handleBlur} value={values.description}/>
                 </div>
+                <div className="error mt-0" style={{color:"red"}}>{touched.description &&  errors.description?errors.description:""}</div>
                 <div className="col-12">
-                    <label for="Price" className="form-label">Price</label>
-                    <input onChange={(e) => { setProduct({ ...product, price: e.target.value }) }} name='price' type="number" min="10" className="form-control" id="Price" placeholder="price" />
+                    <label htmlFor="Price" className="form-label">Price</label>
+                    <input  name='price' type="number"  className="form-control" id="Price" placeholder="price" onChange={handleChange} onBlur={handleBlur} value={values.price}/>
                 </div>
+                <div className="error mt-0" style={{color:"red"}}>{touched.price &&  errors.price?errors.price:""}</div>
                 {/* <div className="col-12">
                 <label for="Image" className="form-label">Upload Image</label>
                 <input onChange={(e)=>{setProduct({...product,image:e.target.value})}} type="file" className="form-control" id="Image" placeholder="image" />
             </div> */}
                 <div className=" col-12">
-                    <label for="Category" className="form-label">Category</label>
-                    <select onChange={(e) => { setProduct({ ...product, category: e.target.value }) }} name='category' id="Category" className="form-select">
-                        <option selected>Choose...</option>
+                    <label htmlFor="Category" className="form-label">Category</label>
+                    <select  name='category' id="Category" className="form-select" onChange={handleChange} onBlur={handleBlur} value={values.category}>
+                        <option value="">Choose...</option>
                         <option>Electronics</option>
                         <option>Men's Clothing</option>
                         <option>Jewelery</option>
@@ -48,6 +59,7 @@ function AddProducts() {
                         <option>Other</option>
                     </select>
                 </div>
+                <div className="error mt-0" style={{color:"red"}}>{touched.category &&  errors.category?errors.category:""}</div>
 
                 <div className="col-12">
                     <button type="submit" className="btn btn-primary">Save Product</button>
