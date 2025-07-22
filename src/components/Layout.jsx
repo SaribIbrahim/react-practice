865af88e296f637703886c478ceb81d3c2404f4a
Sarib Ibrahim <sarib.ibraheem3001@gmail.com>
2025-07-22 18:47:27 +0500

    Summed all products prices using reduce function

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Nav from './Nav'
 import { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 function Layout() {
 
   let [productsAPI, setProductsAPI] = useState([]);
+  const {cart}=useSelector((state)=>state.rootReducer);
+  const totalPrice = cart.cart.reduce((sum, item) => sum + item.price, 0);
 
   let getData= async()=>{
     try{
@@ -25,7 +28,17 @@ function Layout() {
   return (
     <>
     <Nav products={productsAPI} />
-    <Outlet />
+   <div className="row">
+     <div className="col-9">
+      <Outlet />
+     </div>
+     <div className="col-3">
+      <div className='Price'>
+       <h2>Total Price</h2>
+       <h3>${totalPrice.toFixed(2)}</h3>
+     </div>
+     </div>
+   </div>
     </>
   )
 }
